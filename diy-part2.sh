9fc75d852eaccea423b2c637f616d0f506a705dc
3dudu <3dudu@126.com>
2023-09-19 08:37:19 +0800

    Update diy-part2.sh
    packages

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,8 @@ sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./inc
 sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-6.1-/g' ./include/image.mk
 sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt-Firker '/g" ./package/lean/default-settings/files/zzz-default-settings
 sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-SE'/g" ./package/base-files/files/bin/config_generate
+sed -i "s/\"http:/window.location.protocol+\"/g" ./feeds/luci/luci-app-netdata/luasrc/view/netdata/netdata.htm
+
 # curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
 # curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
 # curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/logo.jpg > .package/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
@@ -19,6 +21,7 @@ sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-SE'/g" ./package/base-files/files
 sed -i 's/192.168.1.1/192.168.1.6/g' package/base-files/files/bin/config_generate
 # rm -rf ./feeds/luci/applications/luci-app-qbittorrent
 rm -rf ./feeds/luci/applications/luci-app-serverchan
+rm -rf ./feeds/luci/applications/luci-app-mosdns
 # rm -rf ./package/diy-ziyong/adguardhome
 rm -rf ./feeds/packages/net/adguardhome
 rm -rf ./feeds/packages/net/smartdns
@@ -27,7 +30,7 @@ rm -rf ./feeds/luci/applications/luci-app-wrtbwmon
 rm -rf .package/sirpdboy-package/smartdns
 rm -rf .package/sirpdboy-package/adguardhome
 rm -rf .package/sirpdboy-package/luci-app-dockerman
-rm -rf .package/sirpdboy-package/luci-app-autotimeset
+#rm -rf .package/sirpdboy-package/luci-app-autotimeset
 # rm -rf ./feeds/packages/net/https-dns-proxy
 # svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
 # rm -rf feeds/packages/lang/golang
